c923034ff763fad14ee9dc26f76a0ee071475559
pydev8624 <irajmirzazadeh2024@gmail.com>
2025-01-31 17:56:00 +0330

    Update code2.asm

--- code2.asm
+++ code2.asm
@@ -1,7 +1,14 @@
 section .text
     global _start
 _start:
+
     ; System call: exit(code=0)
+
     mov rax, 60         ; System call number for exit
-    xor rdi, rdi        ; Exit code 0
-    syscall             ; Invoke the system call    
+    xor rdi, rdi        ; Exit code 0 clear output
+
+    ; 0110 1100
+    ; 0110 1100
+    ; 0000 0000 --> ZF=1
+
+    syscall             ; Invoke the system call    32-bit int 0x80
